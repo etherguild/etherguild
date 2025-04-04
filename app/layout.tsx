5438da5878668d4bed905198810d5aad43e1592e
Kevin Weaver <kevinweaver2@gmail.com>
2025-04-03 22:18:44 -0700

    delete comment

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,6 @@ export default function RootLayout({
           attribute="class"
           defaultTheme="dark"
           forcedTheme="dark"
-          //enableSystem
           disableTransitionOnChange
         >
           <Providers>
